1d9a1b33b98b7d37f27a9ad4b613266f54d1c75a
Dog <104234930+dgxo@users.noreply.github.com>
2025-07-14 22:59:24 +0100

    Add cwd field to pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -4,5 +4,6 @@ module.exports = {
   interpreter: "bun", // Bun interpreter
   env: {
     PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
-  }
+  },
+  cwd: "~/dovedale-map"
 };
