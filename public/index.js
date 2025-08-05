f196197afb8422896d3e5e5908784f1e18d2ad93
Sectly <67114413+Sectly@users.noreply.github.com>
2025-08-05 14:58:50 +0200

    remove service worker

--- public/index.js
+++ public/index.js
@@ -962,17 +962,6 @@ elements.reconnectBtn.addEventListener("click", () => {
 	attemptReconnect();
 });
 
-// Setup Service Worker
-if ('serviceWorker' in navigator) {
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/sw.js').then(registration => {
-			console.log('SW registered: ', registration);
-		}).catch(registrationError => {
-			console.log('SW registration failed: ', registrationError);
-		});
-	});
-}
-
 // Initialize Application
 const init = () => {
 	trackTransforms();
